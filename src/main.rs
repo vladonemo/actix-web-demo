f72b7b2d57ce254b439d49465b14b0fe391dc7a2
Vladimir Nemergut <vladimir.nemergut@siemens-healthineers.com>
2020-10-28 14:48:42 +0100

    Step 5: add a route manually
    (cherry picked from commit d2991d9f0e7b5c0424a2c3d458df8c202fce3732)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, get, post, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, post, HttpResponse, HttpServer, Responder, web};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -10,12 +10,18 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+
     })
         .bind("0.0.0.0:8080")?
         .run()
