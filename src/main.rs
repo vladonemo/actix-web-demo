e81d271ecb571e41fe6d7ae50b41b9d9ff352ae0
Vladimir Nemergut <vladimir.nemergut@siemens-healthineers.com>
2020-10-28 14:40:23 +0100

    Step 4: add post endpoint
    (cherry picked from commit 1ebac61cf01a1eab51ac32eb6f306e4a0a8b5b87)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, post, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
 #[actix_web::main]
@@ -10,6 +15,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(echo)
     })
         .bind("0.0.0.0:8080")?
         .run()
