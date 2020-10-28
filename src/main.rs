e7081a2dba2d01bf90bae968af9114c305c83201
Vladimir Nemergut <vladimir.nemergut@siemens-healthineers.com>
2020-10-28 14:37:22 +0100

    Step 3: add first endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
